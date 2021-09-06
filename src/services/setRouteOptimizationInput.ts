4393ec87568a5b4e9744bd0bc37fff5a578c6091
Carlos Gallo <carlos.gallo@somagrupo.com.br>
2021-09-06 10:05:55 -0300

    firts attempt to create jobs object

--- src/services/setRouteOptimizationInput.ts
+++ src/services/setRouteOptimizationInput.ts
@@ -8,29 +8,44 @@ interface setRouteOptimizationInputProps{
 }
 
 export function setRouteOptimizationInput(baseCoordinates:coordinates,pointsCoordinates:coordinates[]){
+if(baseCoordinates && pointsCoordinates){
+   
 
-    // const jobs = pointsCoordinates.map(
+    }
 
-    // )
-    const vehicles = [{
-        "id": 1, 
-        "profile": "driving-car",
-        "start":[
-            baseCoordinates.latitude,
-            baseCoordinates.longitude
-        ],
-        "end":[
-            baseCoordinates.latitude,
-            baseCoordinates.longitude
+    const jobs = pointsCoordinates.map((pointCoordinates, index)=>{
+        id: index,
+        service: 300,
+        location:[
+            pointCoordinates.latitude,
+            pointCoordinates.longitude
         ]
+    })
+
+        const vehicles = [{
+            "id": 1, 
+            "profile": "driving-car",
+            "start":[
+                baseCoordinates.latitude,
+                baseCoordinates.longitude
+            ],
+            "end":[
+                baseCoordinates.latitude,
+                baseCoordinates.longitude
+            ]
+            
+            
+        }]
+        
+        // console.log('baseCoordinates: ', typeof baseCoordinates, baseCoordinates)
+        
+        // console.log('pointsCoordinates: ', typeof pointsCoordinates, pointsCoordinates)
+        
+        // console.log('vehicles: ', vehicles)
+        console.log('pointsCoordinates: ', pointsCoordinates)
+        console.log('jobs: ', jobs)
 
 
-    }]
-
-    console.log('baseCoordinates: ', typeof baseCoordinates, baseCoordinates)
-    
-    console.log('pointsCoordinates: ', typeof pointsCoordinates, pointsCoordinates)
-
-    console.log('vehicles: ', vehicles)
-    
+     
+    }
 }
